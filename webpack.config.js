d40bb230d1dceaa260e8521d64c9128f76b1e7bb
RFaisal458 <faisalghozi458@gmail.com>
2020-08-10 13:57:14 +0700

    memberbaiki/menambahkan fitur untuk bisa menambahkan gambar dan memperbaiki dev server dan lainnya

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Untuk render sass
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // Untuk render html
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack"); // to access built-in plugins
 
 module.exports = {
     mode: "development",
-    
-    devtool: "inline-source-map",/*When webpack bundles your source code, it can become difficult to track down errors and warnings to their original location. For example, if                                      you bundle three source files (a.js, b.js, and c.js) into one bundle (bundle.js) and one of the source files contains an error, the stack                                        trace will simply point to bundle.js. This isn't always helpful as you probably want to know exactly which source file the error came from*/
-    
+
+    devtool: "inline-source-map",
+    /*When webpack bundles your source code, it can become difficult to track down errors and warnings to their original location. For example, if                                      you bundle three source files (a.js, b.js, and c.js) into one bundle (bundle.js) and one of the source files contains an error, the stack                                        trace will simply point to bundle.js. This isn't always helpful as you probably want to know exactly which source file the error came from*/
+
     entry: ["./src/js/script.js", "./src/sass/main.scss"], // Entry File yang akan di gunakan atau template yang akan di gunakan
-    devServer: { // Untuk membuat server lokal
-        contentBase: path.join(__dirname, "dist"),
+    devServer: {
+        // Untuk membuat server lokal
+        contentBase: [path.join(__dirname, "dist"), path.join(__dirname, "src")],
         compress: true,
         watchContentBase: true,
-        port: 9000,
+        port: 4580,
+        hot: true,
+        inline: true,
+        watchOptions: {
+            poll: true,
+            ignored: "/node_modules/"
+        },
+        open: true
     },
     output: {
         path: path.resolve(__dirname, "./dist/"),
-        filename: "js/script.js" // Output dari javascriptnya
+        filename: "js/script.js", // Output dari javascriptnya
     },
     module: {
         rules: [{
@@ -24,15 +35,25 @@ module.exports = {
                 use: [{
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: "./dist/",
+                            publicPath: "./css/",
                         },
                     },
                     "css-loader",
-                    "sass-loader" // kita akan menggunakan sass loader untuk convert sass ke css
+                    "sass-loader", // kita akan menggunakan sass loader untuk convert sass ke css
                 ],
             },
             {
-                test: /\.js?$/, // rule untuk js
+                test: /\.(png|jpe?g|svg|gif|ico)$/,
+                exclude: /node_modules/,
+                loader: 'url-loader?name=images/[name].[ext]'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                exclude: /node_modules/,
+                loader: 'url-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(es6|js|jsx)$/, // rule untuk js
                 exclude: /node_modules/,
                 loader: "babel-loader", // menggunakan babel loader
                 query: {
@@ -42,12 +63,16 @@ module.exports = {
         ],
     },
     plugins: [
+        new webpack.ProgressPlugin(), // untuk mengetahui sejauh mana progress compiled
         new MiniCssExtractPlugin({
             filename: "./css/style.css", // output nama apa yang kita inginkan dari sass yang di atas
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src/html/index.html"),// template html mana yang akan kita render
-            filename: path.resolve(__dirname, "dist/index.html")
-        })
+            template: path.resolve(__dirname, "src/html/index.html"), // template html mana yang akan kita render
+            filename: path.resolve(__dirname, "dist/index.html"), // file yang akan di hasilkan
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [],
+        }),
     ],
 };
